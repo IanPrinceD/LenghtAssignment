bfaf28a36a5a054c579d494e3890d16eca9edd26
IanPrinceD <princedumanhug@gmail.com>
2023-09-02 14:57:34 +0800

    Second Update (character spelling)

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-var chracterCount = prompt ("What can you say about me? (please limit it into 150 characters)");
-    alert ("You have written " + chracterCount.length +" characters" +"you only have " + (150-chracterCount.length) + " remaining characters");
+var characterCount = prompt ("What can you say about me? (please limit it into 150 characters)");
+    alert ("You have written " + characterCount.length +" characters" +"you only have " + (150-characterCount.length) + " remaining characters");
