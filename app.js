3035001618c26ca803d23ade508a1d8d813ca456
IanPrinceD <princedumanhug@gmail.com>
2023-09-02 14:32:45 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,2 @@
+var chracterCount = prompt ("What can you say about me? (please limit it into 150 characters)");
+    alert ("You have written " + chracterCount.length +" characters" +"you only have " + (150-chracterCount.length) + " remaining characters");
