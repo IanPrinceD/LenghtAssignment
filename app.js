6253a6085cc98bfee07b2c2399f39d224dfe51c0
IanPrinceD <princedumanhug@gmail.com>
2023-09-02 15:02:22 +0800

    Third update (prompt)

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-var characterCount = prompt ("What can you say about me? (please limit it into 150 characters)");
+var characterCount = prompt ("Type anything you want. (please limit it into 150 characters)");
     alert ("You have written " + characterCount.length +" characters" +"you only have " + (150-characterCount.length) + " remaining characters");
